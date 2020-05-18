13cc9839604789bac7431e563cef0c8beac673aa
VanillaOperator <65506738+VanillaOperator@users.noreply.github.com>
2020-05-18 02:02:04 -0300

    About 'a tiny amount' bug
    Hello,
    You should be more careful with V[21], because it's an auxiliary variable used for different purposes in each common event . For the common event 0083, where cumInside() is called, V[21] store the amount of cum expelled in the most recent ejaculation, just the value you calculated and stored in ejaculation_delta. So there's no need to use V[700].
    And because you used V[21] to store another value, the event 0083 fails to use the right notification of cum amount, since it needs V[21] to find the right index for data_H "in_notify":[].

--- src/MOD_realisticEjaculations.js
+++ src/MOD_realisticEjaculations.js
@@ -2,66 +2,26 @@
     @author: nwishmaster <nwishmaster@mailbox.org>
     @version: 0.9.0
     @description "Realistic Ejaculation MOD" adds creampies possibility to leak through vagina into uterus
-
-    Note that this mod uses game variable #700
  */
 (function () {
-    /* Drop variable 700 after sex event */
-    const _Scene_Boot_start = Scene_Boot.prototype.start;
-    Scene_Boot.prototype.start = function () {
-        _Scene_Boot_start.apply(this, arguments);
-        overrideDatabase("$dataCommonEvents", {
-            "id": 84,
-            "list": [
-                { "code": 108, "indent": 0, "parameters": ["精液処理"] },
-                { "code": 122, "indent": 0, "parameters": [10, 10, 1, 1, 76] },
-                { "code": 122, "indent": 0, "parameters": [10, 10, 1, 1, 78] },
-                { "code": 122, "indent": 0, "parameters": [11, 11, 1, 1, 77] },
-                { "code": 122, "indent": 0, "parameters": [50, 50, 1, 1, 76] },
-                { "code": 122, "indent": 0, "parameters": [50, 50, 1, 1, 78] },
-                { "code": 122, "indent": 0, "parameters": [51, 51, 1, 1, 77] },
-                { "code": 122, "indent": 0, "parameters": [49, 49, 1, 0, 1] },
-                { "code": 108, "indent": 0, "parameters": ["recently精液リセット"] },
-                { "code": 121, "indent": 0, "parameters": [85, 85, 1] },
-                { "code": 121, "indent": 0, "parameters": [86, 86, 1] },
-                { "code": 122, "indent": 0, "parameters": [76, 78, 0, 0, 0] },
-                { "code": 122, "indent": 0, "parameters": [73, 73, 0, 0, 0] },
-                { "code": 122, "indent": 0, "parameters": [382, 382, 0, 0, 0] },
-                { "code": 122, "indent": 0, "parameters": [700, 700, 0, 0, 0] },
-                { "code": 0, "indent": 0, "parameters": [] }
-            ],
-            "name": "Semen Assignment/Semen Reset",
-            "switchId": 1,
-            "trigger": 0
-        });
-    };
-
     const _cumInsideFn = prostitute.cumInside;
     prostitute.cumInside = () => {
-        const INFO_STATIC_TEXT = 'ml ejaculated to vagina, \n\x1bc[1]\x1bv[21]\x1bc[0] sperm reach \x1bn[1]\'s uterus';
         const V = $gameVariables._data || {};
         const IS_IN_VAGINA = V[328] === 1;
         const WITH_CONDOM = V[86] === 1;
-        const EJACULATED_AMOUNT = V[77];
-        const LAST_EJACULATED_AMOUNT = V[700] || 0;
-        let ejaculation_delta = EJACULATED_AMOUNT - LAST_EJACULATED_AMOUNT;
-
-        if (ejaculation_delta < 0) {
-            ejaculation_delta = 0;
-        }
+        let sperm_count = 0;
 
         _cumInsideFn.apply(this, arguments);
 
-        if (IS_IN_VAGINA && !WITH_CONDOM && ejaculation_delta >= 3) {
-            V[21] = ejaculation_delta * randmm(75, 150) + randmm(1, 100);
+        if (IS_IN_VAGINA && !WITH_CONDOM && V[21] >= 3) {
+            sperm_count = V[21] * randmm(75, 150) + randmm(1, 100);
 
-            if (V[21] >= 1) {
-                bun(`From ${ejaculation_delta}` + INFO_STATIC_TEXT);
+            if (sperm_count >= 1) {
+                bun('From \x1bv[21]ml ejaculated to vagina, \n\x1bc[1]' + `${sperm_count}` + '\x1bc[0] sperm reach \x1bn[1]\'s uterus');
             }
 
-            V[13] += V[21];
-            V[53] += V[21];
-            V[700] = V[77];
+            V[13] += sperm_count;
+            V[53] += sperm_count;
         }
     }
 })();
